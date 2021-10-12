e7a05fa234ccb831b3a75e98bfb45f2af6715898
codegrabber1 <59266755+makecodework@users.noreply.github.com>
2021-10-12 20:27:41 +0300

    uupdate

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ function initMap() {
   const uluru = { lat: 50.084265, lng: 8.661599 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 3,
     center: uluru,
   });
   // The marker, positioned at Uluru
@@ -12,7 +12,10 @@ function initMap() {
   const marker = new google.maps.Marker({
     position: uluru,
     map: map,
-    icon: `${iconFolder}location.svg`,
+    animation: google.maps.Animation.DROP,
+    icon: {
+      url: `${iconFolder}location.svg`,
+    },
   });
 }
 // #Google map.
