341167e4bb4a0a6b930830882e97fc1d6e07ef0d
Vladislav Nazarenko <vnazarenko@codeberry.de>
2021-10-22 14:50:03 +0200

    upd: index.html - map zoom

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
 // Google map.
 function initMap() {
-  // The location of Uluru
-  const uluru = { lat: 50.084265, lng: 8.661599 };
+  // The location of Buergerwiese
+  const bw = { lat: 50.084265, lng: 8.661599 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 6,
-    center: uluru,
+    zoom: 15,
+    center: bw,
   });
-  // The marker, positioned at Uluru
+  // The marker, positioned at Buergerwiese
   const iconFolder = "assets/img/";
   const marker = new google.maps.Marker({
-    position: uluru,
+    position: bw,
     map: map,
     animation: google.maps.Animation.DROP,
     icon: {
